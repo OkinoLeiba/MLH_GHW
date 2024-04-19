87a954d9cc082274c80bb3479c7bc057ee41f889
Tech Fleet Language Project <147744718+tech-fleet-language-project@users.noreply.github.com>
2024-04-19 23:00:09 +0100

    update functions

--- mlh_stripe.sh
+++ mlh_stripe.sh
@@ -2,15 +2,18 @@
 
 API_KEY="sk_test_51P2ITx08E6dS14v5umSelPiMlsmiRmUipXA4TAcQZfyXDx59M6qEU2ptPcGmzOsFab4UDxfGWcSaXqInfgOUqdoq00ybu4R8fY"
 
-delecare -A request 
+declare -A request 
 
 url="https://raw.githubusercontent.com/stripe-samples/test-data/master/customer-with-subscription/create-fixtures.json"
 
-function resqest() {
-    curl -X GET -H "Content-type: application/json" -H "Accept: application/json" -H "Authorization: ${API_KEY}"\
+function request() {
+    curl -X GET ${url} -H "Content-type: application/json" -H "Accept: application/json" -H "Authorization: ${API_KEY}"\
     $url >> request
 }
 
 function create_customer(){
-    curl https://api.stripe.com/v1/customers -u "${API_KEY}:"
+    curl https://api.stripe.com/v1/customers -u "${API_KEY}"\
+    -d "name={}&email={}"
 }
+
+request
