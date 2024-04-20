f9b6221659b99abc36f4ea0112fb512f9d11a461
Okino Kamali Leiba <17099286+OkinoLeiba@users.noreply.github.com>
2024-04-20 01:08:22 +0100

    Update mlh_stripe.cs

--- mlh_stripe.cs
+++ mlh_stripe.cs
@@ -6,7 +6,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Collection.Generic;
 
-const API_KEY = StripeConfiguration.API_KEY = "sk_test_51P2ITx08E6dS14v5umSelPiMlsmiRmUipXA4TAcQZfyXDx59M6qEU2ptPcGmzOsFab4UDxfGWcSaXqInfgOUqdoq00ybu4R8fY";
+const API_KEY = StripeConfiguration.API_KEY = "";
 
 var options = RequestOption{ApiKey=API_KEY}
 
