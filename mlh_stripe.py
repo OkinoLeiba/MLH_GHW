346c75b7b93d4d18ec02408e655cc3b73818e57a
Okino Leiba <okino.leiba@yahoo.com>
2024-05-18 07:22:08 +0100

    add functionality

--- mlh_stripe.py
+++ mlh_stripe.py
@@ -11,7 +11,7 @@ py_compile.compile('mlh_stripe.py')
 
 app = FastAPI()
 
-def PullData(data_request: Optional[Any] = None) -> any:
+def pull_data(data_request: Optional[Any] = None) -> any:
     file = open('stripe_data.json')
 
     data = json.load(file)
@@ -25,19 +25,29 @@ def PullData(data_request: Optional[Any] = None) -> any:
 
 global API_KEY
 
-API_KEY = stripe.api_key = PullData('apiKey')
+API_KEY = stripe.api_key = pull_data('apiKey')
 
 # unnecessary? 
 stripe.api_key = API_KEY
 
 URL = 'https://raw.githubusercontent.com/stripe-samples/test-data/master/customer-with-subscription/create-fixtures.json'
+
+app.route('/authorize', methods=['POST'])
+def authorize() -> None:
+    stripe.OAuth.authorize_url(scope='read-only', always_prompt=False)
+    
+app.route('/deauthorize', methods=['POST'])
+def deAuthorize() -> None:
+    stripe.OAuth.deauthorize(api_key=API_KEY, client_id='')
+    
+    
 class MLH_Stripe(StripeObject):
 
     def __init__(self) -> None:
         pass
     
     
-    def RequestCall(self, url: Optional[str] = None) -> json:
+    def request_call(self, url: Optional[str] = None) -> json:
         # r = requests.Request('GET', URL)
         header = {
            'Authorization' : API_KEY 
@@ -51,12 +61,11 @@ class MLH_Stripe(StripeObject):
 
 
 
-
     # stripe.app_info('stripe-samples/checkout-one-time-payments', version='0.0.1', url='https://github.com/stripe-samples/checkout-one-time-payments')
 
     # create customer data to [initial test data]
     app.post('/v1/customers')
-    def CreateCustomer(self) -> stripe.Customer:
+    def create_customer(self) -> stripe.Customer:
         customer_params =  {
                 'id'  :  "cus_NffrFeUfNV2Hib",
                 'address'  :  None,
@@ -112,7 +121,7 @@ class MLH_Stripe(StripeObject):
     
     # search for customer: other than id parameter
     app.get(f'/v1/customers/search')
-    def QueryCustomer(self, param: Optional[list[Any] | str] = None, param_type: Optional[list[Any] | str] = None) -> stripe.Customer:
+    def query_customer(self, param: Optional[list[Any] | str] = None, param_type: Optional[list[Any] | str] = None) -> stripe.Customer:
         params = ''
         while True:
             if type(param) == str:
@@ -129,7 +138,7 @@ class MLH_Stripe(StripeObject):
 
     # request customer data object from strip
     app.get(f'/v1/customers/{id}')
-    def RequestCustomer(self, id: Optional[str] = None) -> stripe.Customer:
+    def request_customer(self, id: Optional[str] = None) -> stripe.Customer:
         return cast(stripe.Customer, stripe.Customer.retrieve(id, api_key=API_KEY))
 
         # "ch_3Lmjoz2eZvKYlo2C1rBER4Dk",
@@ -137,28 +146,30 @@ class MLH_Stripe(StripeObject):
 
     # update customer data object
     app.post(f'/v1/customers/{id}')
-    def UpdateCustomer(self, id: Optional[str] = None, update_option: Optional[dict] = None) -> None:
-        test = [update_key = update_value]
+    def update_customer(self, id: Optional[str] = None, update_option: Optional[dict] = None) -> None:
+        # test = [update_key = update_value]
         metadata = {"order_id": "6735"}
         print(stripe.Customer.modify(id, ))
         return stripe.Customer.modify(id, metadata = {"order_id": "6735"})
 
 
     app.delete(f'/v1/customers/{id}')
-    def DeleteCustomer(self, id: Optional[str] = None) -> None:
+    def delete_customer(self, id: Optional[str] = None) -> None:
         stripe.Customer.delete(id, stripe_account='')
     
 
     app.get('/v1/customers')
-    def GetCustomerList(self, count: int = 0) -> list:
+    def get_customer_list(self, count: int = 0) -> list:
         return stripe.Customer.list(list=count, stripe_account='')
 
 if __name__ == '__main__':
     mlh_stripe = MLH_Stripe()
-    mlh_stripe.CreateCustomer()
-    mlh_stripe.QueryCustomer("Jenny Rosen", 'email')
-    mlh_stripe.RequestCustomer(mlh_stripe.CreateCustomer().id)
-    mlh_stripe.UpdateCustomer(mlh_stripe.CreateCustomer().id)
+    mlh_stripe.create_customer()
+    mlh_stripe.query_customer("Jenny Rosen", 'email')
+    mlh_stripe.request_customer(mlh_stripe.create_customer().id)
+    mlh_stripe.update_customer(mlh_stripe.create_customer().id)
+    # app.get()
+    # app.run()
     
 
 
